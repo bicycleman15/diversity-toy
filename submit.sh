a5886984dd741f79a9892328454bc729daa64007
Jatin Prakash <jatinprakash1511@gmail.com>
2025-09-01 19:25:47 -0400

    add sweep

--- submit.sh
+++ submit.sh
@@ -1,25 +1,19 @@
-python toy_example.py \
-vocab_size=100 \
-num_iterations=1001 save_every=250 \
-pretrain_policy.enable=False \
-reward_type="bimodal-halves-binary-10" \
-loss_cfg.advantage_type="raw" \
-loss_cfg.kl_grad_type="rev-vanilla" \
-loss_cfg.entropy_grad_type="analytical" \
-compute_kl_to_target=True \
-entropy_coeff=0.0 \
-kl_coeff=1e-1 \
-ref_pol_eps=1e-20 \
-batch_size=32 \
-learning_rate=5e-3 \
-grad_clip=100.0 \
-seed=10 \
-device=0
+#!/bin/bash
+#SBATCH --job-name=diversity
+#SBATCH --open-mode=append
+#SBATCH --output=/gpfs/home/jp7467/slurm_logs/%j_%x.out
+#SBATCH --error=/gpfs/home/jp7467/slurm_logs/%j_%x.err
+#SBATCH --export=ALL
+#SBATCH --time=2:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=a100_dev,a100_short,a100_long
 
-rev-vanilla
-fwd-simple
+conda activate
+conda activate rl
 
-##### hydra sweep
+pwd
 
 python toy_example.py -m \
 \
@@ -38,4 +32,10 @@ batch_size=32 \
 learning_rate=5e-3 \
 grad_clip=100.0 \
 seed=10 \
-device=1
+device=0
+
+
+
+echo "Run finished at: "
+date
+exit
